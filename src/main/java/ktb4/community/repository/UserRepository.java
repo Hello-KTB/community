a68bf17fbb8f61e7d2875ab2893161616369e329
7angJung <peterj0117@naver.com>
2026-06-03 18:30:02 +0900

    feat: Query메서드 작성(이메일, 닉네임 존재 여부)

--- src/main/java/ktb4/community/repository/UserRepository.java
+++ src/main/java/ktb4/community/repository/UserRepository.java
@@ -6,4 +6,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    // 특정 이메일이 존재하는지 확인
+    boolean existsByEmail(String email);
+
+    // 특정 닉네임이 존재하는지 확인
+    boolean existsByNickname(String nickname);
 }
