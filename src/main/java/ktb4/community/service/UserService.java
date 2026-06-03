e276aaf4f2f736726b95c2c0ada408753ed7fb46
7angJung <peterj0117@naver.com>
2026-06-03 14:57:12 +0900

    feat: UserService제작

--- src/main/java/ktb4/community/service/UserService.java
+++ src/main/java/ktb4/community/service/UserService.java
@@ -1,37 +1,44 @@
-/*
-import ktb4.community.dto.request.UserRequestDto;
-import ktb4.community.dto.response.UserResponseDto;
+package ktb4.community.service;
+
+import ktb4.community.entity.User;
+import ktb4.community.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Date;
-
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
-    public UserResponseDto createUser(UserRequestDto requestDto) {
-        User user = new User(
-                requestDto.getUserEmail(),
-                requestDto.getUserPassword(),
-                requestDto.getUserNickname()
-        );
-        user.setUserCreatedAt(new Date());
-        user.setUserUpdatedAt(new Date());
-        SecurityProperties.User savedUser = userRepository.save(user);
-        return new UserResponseDto(savedUser);
+    public User create(String email, String password, String nickname, String image) {
+        User user = new User(email, password, nickname, image);
+        return userRepository.save(user);
     }
 
-    public UserResponseDto getUser(Long userId) {
-        User user = userRepository.findByUserId(userId);
-        return new UserResponseDto(user);
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다"));
     }
 
-    public void deleteUser(Long userId) {
-        userRepository.deleteByUserId(userId);
+    public User getReferenceById(Long id) {
+        return userRepository.getReferenceById(id);
     }
-}*/
+
+    @Transactional
+    public User update(Long id, String nickname) {
+        User user = findById(id);
+        if (nickname != null) {
+            user.changeNickname(nickname);
+        }
+        return user;
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        // 프록시 반환 (접근 시 초기화)
+        userRepository.delete(findById(id));
+    }
+
+}
