69bc14be15ad70ee36f21168ca87e3967b375c26
7angJung <peterj0117@naver.com>
2026-06-07 16:39:12 +0900

    feat: 프론트엔드 연동

--- src/main/java/ktb4/community/config/CorsConfig.java
+++ src/main/java/ktb4/community/config/CorsConfig.java
@@ -1,4 +1,17 @@
 package ktb4.community.config;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
